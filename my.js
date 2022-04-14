969061989c3102b4f8bf55b472e7d8de6f14a2cf
enerjio <75221271+enerjio@users.noreply.github.com>
2022-04-14 13:24:43 +0300

    Update my.js

--- my.js
+++ my.js
@@ -1,7 +1,7 @@
 
 function testGS(){
 
-    const url ="https://script.googleusercontent.com/macros/echo?user_content_key=-SQ9cMhiE4LdPubqmFwI-l8bmhR8izDqPrA66gDbUHwVMk6E403hk_-HTruNKeEPAj4hXcSeK6Hrf269PgyBc2ODKyb2ODvIm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBxEoIptzfrYg0I_K-JJriYTABK2upRuO6BymXlW4n3BXgZ0Dn8Q0WLypHnojptp-4RH8Oj9w8J6&lib=MYl1DWJOg9utvYEagXVRMJk4PFTUaRp0z";
+    const url ="https://script.google.com/macros/s/AKfycbxyvTwQg2lCBn2pHWPhItvfyUh7gEYgncy5MJefDZ3MGWMCBkjQ/exec";
 
 
     fetch(url)
@@ -12,4 +12,30 @@ function testGS(){
 
 }
 
+
+function addGS(){
+
+    const url ="https://script.google.com/macros/s/AKfycbxyvTwQg2lCBn2pHWPhItvfyUh7gEYgncy5MJefDZ3MGWMCBkjQ/exec";
+
+
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'omit', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"Olivia"}) // body data type must match "Content-Type" header
+      });
+      
+
+}
+
+document.getElementById("btn2").addEventListener("click",addGS);
+
 document.getElementById("btn").addEventListener("click",testGS);
+
